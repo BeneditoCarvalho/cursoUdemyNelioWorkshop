48c39a7d7320ee2874cc5dada4633528bccfb93d
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-16 11:05:56 -0300

    Department Service

--- module-info.java
+++ module-info.java
@@ -1,8 +1,13 @@
 	module workshop.javafx.jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml, javafx.controls;
 	
 	opens gui to javafx.graphics, javafx.fxml, javafx.controls;
+	
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.controls, javafx.base;
+	
+	opens model.services to javafx.graphics, javafx.fxml, javafx.controls, javafx.base;
 }
