54c17af25c6bf07d8a1fd92240e57e122ac7b8b5
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-17 21:43:11 -0300

    Seller TableView

--- module-info.java
+++ module-info.java
@@ -4,13 +4,15 @@
 	requires javafx.base;
 	requires java.sql;
 	
-	opens application to javafx.graphics, javafx.fxml, javafx.controls;
+	opens application to javafx.graphics, javafx.fxml, javafx.controls, javafx.base;
 	
-	opens gui to javafx.graphics, javafx.fxml;
+	opens gui to javafx.graphics, javafx.fxml, javafx.controls, javafx.base;
 	
 	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 	
 	opens model.services to javafx.graphics, javafx.fxml, javafx.base;
 	
-	opens db to java.sql;
+	opens db to java.sql, javafx.graphics, javafx.fxml, javafx.controls, javafx.base;
+	
+	opens gui.util to java.sql, javafx.graphics, javafx.fxml, javafx.controls, javafx.base;
 }
